596b2610ba96f2d3a82f38b3eaac1f9ba9e860ed
JakubWachowicz <wachowiczkuba23@gmail.ocm>
2023-09-27 23:38:15 +0200

    Sliders added

--- lib/widgets/my_dropdown.dart
+++ lib/widgets/my_dropdown.dart
@@ -1,22 +1,51 @@
-import 'dart:js_interop';
-
 import 'package:flutter/material.dart';
 
 class MyDropdown extends StatefulWidget {
-  MyDropdown({Key? key,required this.list}) : super(key: key);
+  MyDropdown({Key? key, required this.title, required this.list})
+      : super(key: key);
 
+  String title;
   List<String> list;
+  late String currentValue;
+
   @override
   State<MyDropdown> createState() => _MyDropdownState();
 
-  addToList(String value){
-    !list.isNull? list.add(value):null;
+  addToList(String value) {
+    list.add(value);
   }
 }
 
 class _MyDropdownState extends State<MyDropdown> {
   @override
+  void initState() {
+    super.initState();
+    widget.currentValue = widget.list.first;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DropdownMenu<String>()
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+        Text(widget.title),
+        SizedBox(height: 5,),
+        DropdownMenu<String>(
+          width: 250,
+          initialSelection: widget.list.first,
+          onSelected: (String? value) {
+            setState(() {
+              widget.currentValue = value!;
+            });
+          },
+          dropdownMenuEntries:
+              widget.list.map<DropdownMenuEntry<String>>((String value) {
+            return DropdownMenuEntry<String>(value: value, label: value);
+          }).toList(),
+        )
+      ]),
+    );
   }
 }
--- lib/widgets/options_bar.dart
+++ lib/widgets/options_bar.dart
@@ -1,21 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:reimagine_mobile/widgets/my_dropdown.dart';
 
-class NavBar extends StatelessWidget {
-  const NavBar({Key? key}) : super(key: key);
+
+class OptionsBar extends StatefulWidget {
+  OptionsBar({Key? key}) : super(key: key);
+
+  @override
+  State<OptionsBar> createState() => _OptionsBarState();
+  var currentSliderOneValue = 0.2;
+  var currentSliderTwoValue = 0.6;
+  var currentSliderThreeValue = 1.0;
+  MyDropdown dropdown = MyDropdown(title:"Wybór 1",list: ["Opcja 1","Opcja 2","Opcja 3","Opcja 4"]);
+}
+
+class _OptionsBarState extends State<OptionsBar> {
+
 
   @override
   Widget build(BuildContext context) {
+
+
+    getSelectedSet(){
+      return widget.dropdown.currentValue;
+    }
+
+
+    double height =  MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    _builLogo(){
+      return SizedBox(
+        height: height/6,
+        child: DrawerHeader(child:Container(
+
+
+          alignment: Alignment.centerLeft,
+          child: Text("ReImage",style: TextStyle(fontSize: 24,fontWeight: FontWeight.w700),),
+        )),
+      );
+    }
+
+
+    _buildSliders(){
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                Text("Opcja 1:  " + double.parse((widget.currentSliderOneValue).toStringAsFixed(2)).toString(),),
+                Slider(value: widget.currentSliderOneValue , onChanged: (double value){
+                  setState(() {
+                    widget.currentSliderOneValue = value;
+                  });
+                },min: 0,max: 1,label: "Opcja 1: " + widget.currentSliderOneValue.toString(),),
+              ]),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Opcja 1:  " + double.parse((widget.currentSliderTwoValue).toStringAsFixed(2)).toString(),),
+                    Slider(value: widget.currentSliderTwoValue , onChanged: (double value){
+                      setState(() {
+                        widget.currentSliderTwoValue = value;
+                      });
+                    },min: 0,max: 1,label: "Opcja 1: " + widget.currentSliderTwoValue.toString(),),
+                  ]),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Opcja 1:  " + double.parse((widget.currentSliderThreeValue).toStringAsFixed(2)).toString(),),
+                    Slider(value: widget.currentSliderThreeValue , onChanged: (double value){
+                      setState(() {
+                        widget.currentSliderThreeValue = value;
+                      });
+                    },min: 0,max: 1,label: "Opcja 1: " + widget.currentSliderThreeValue.toString(),),
+                  ]),
+            ),
+
+          ],
+        ),
+      );
+    }
+
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('Profile'),
-            onTap: () {
-
-            },
+          _builLogo(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: widget.dropdown,
           ),
+          _buildSliders()
+
+
+
         ],
       ),
     );
